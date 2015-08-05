6b3bac88f44e8192fe8a258b9800b63f5808970d
iwmr <iwmr@users.noreply.github.com>
2015-08-06 02:19:18 +0900

    Make code shorter

--- disney_com/disney_com_from_abroad.js
+++ disney_com/disney_com_from_abroad.js
@@ -1,24 +1,19 @@
 // ==UserScript==
-// @name               [disney.com] from abroad
-// @namespace          https://github.com/iwmr/
-// @description        redirect URL with "intoverride=true"
+// @name               Disney.com from abroad
+// @namespace          http://maihamakyo.org/
+// @description        add "intoverride=true" to URL
 // @include            http://*disney.com/*
 // @version            0.1
 // ==/UserScript==
-
 (function() {
-    var url_protocol = location.protocol;
-    var url_host = location.host;
-    var url_pathname  = location.pathname ;
-    var url_parameter = location.search;
-    var url_hash = location.hash;           
-    if ( url_parameter.indexOf('intoverride') == -1) {
-        if ( url_parameter.indexOf('?') == -1) {
-            url_parameter = url_parameter + "?intoverride=true";
-        }else{
-            url_parameter = url_parameter + "&intoverride=true";
-        }
-        var new_url = url_protocol + "//" + url_host +  url_pathname +  url_parameter + url_hash;
-        location.href = new_url;
+    var p = location.protocol;
+    var h = location.host;
+    var t = location.pathname;
+    var s = location.search;
+    var a = location.hash;           
+    if ( s.indexOf('intoverride') == -1) {
+        if ( s.indexOf('?') == -1) { s = s + "?intoverride=true"; }else{ s = s + "&intoverride=true"; }
+        var u = p + "//" + h +  t +  s + a;
+        location.href = u;
     }
 })();
